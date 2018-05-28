de2f75dd8cc380979a3ffdc53361a708575f4d98
Ismail Ozsaygi <36489581+agneng@users.noreply.github.com>
2018-05-28 21:56:16 +0300

    design changes

--- src/ObjectUtils.cs
+++ src/ObjectUtils.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
 class ObjectUtils
 {
 	//---------------------------------------------------------------------------------
-    // Purpose : Gets same type objects in scene and returns them as an array.
+        // Purpose : Gets same type objects in scene and returns them as an array.
 	//           Type is given with generics.
 	//           Since it is static declared , can be used like -> ObjectManager.GetSameTypeObjectsInScene< type >();
-	//           It returns an array of type.
-    //---------------------------------------------------------------------------------
+        //           It returns an array of type.
+        //---------------------------------------------------------------------------------
 	public static T[] GetSameTypeObjectsInScene< T >()
 	{
 		T[] objs = GameObject.FindObjectsOfType( typeof( T ) ) as T[];
@@ -19,8 +19,8 @@ class ObjectUtils
 	}
 
 	//---------------------------------------------------------------------------------
-    // Purpose : Adds given type / generic array to given list.
-    //---------------------------------------------------------------------------------
+        // Purpose : Adds given type / generic array to given list.
+        //---------------------------------------------------------------------------------
 	public static void AddTypeArrayToTypeList< T >( T[] typeArray , List< T > typeList )
 	{
 		int i;
